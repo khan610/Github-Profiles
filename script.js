22e7c1660a73762ffa8d7caeecc762fd6850d159
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-04-03 18:05:23 +0200

    Update

---
+++ script.js
@@ -0,0 +1,54 @@
+const APIURL = 'https://api.github.com/users/';
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+
+    createUserCard(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createUserCard(user) {
+  const cardHTML = `<div class="card">
+  <img
+    src="${user.avatar_url}"
+    alt="${user.name}"
+    class="avatar"
+  />
+  <div class="user-info">
+    <h2>${user.name}</h2>
+    <p>
+      ${user.bio}
+    </p>
+
+    <ul>
+      <li>${user.followers} <strong>Followers</strong></li>
+      <li>${user.following} <strong>Following</strong></li>
+      <li>${user.public_repos} <strong>Repos</strong></li>
+    </ul>
+
+    <div id="repos"></div>
+
+  </div>
+</div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = '';
+  }
+});
