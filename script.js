cf12f464e15952821d647f7e41e401d6f9eb4bfe
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-04-03 19:57:28 +0200

    Finished

--- script.js
+++ script.js
@@ -9,9 +9,46 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username);
 
     createUserCard(data);
+    getRepos(username);
   } catch (err) {
-    console.log(err);
+    if (err.response.status === 404) {
+      createErrorCard('No profile with this username');
+    }
   }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + '/repos?sort=created');
+
+    addReposToCard(data);
+  } catch (err) {
+    createErrorCard('Problem fetching repos');
+  }
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  repos.slice(0, 10).forEach((repo) => {
+    const repoEl = document.createElement('a');
+    repoEl.classList.add('repo');
+    repoEl.href = repo.html_url;
+    repoEl.target = '_blank';
+    repoEl.innerText = repo.name;
+
+    reposEl.appendChild(repoEl);
+  });
 }
 
 function createUserCard(user) {
